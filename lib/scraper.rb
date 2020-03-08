c58820283be4fd8b1f4de5f4931b311dbf0dcf3f
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-03-08 03:22:27 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,7 +15,12 @@ class Scraper
   end
   
   def make_courses
-    self.get_courses
+    self.get_courses.each do |post|
+      course = Course.new
+      course.title = post.css('h2').text.strip
+      course.schedule = post.css('.date').text.strip
+      course.description = post.ss('p').text.strip
+    end
   end
   
   def print_courses
