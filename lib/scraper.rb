337361a97a537004756212d16917fbd635f8b809
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-03-08 03:18:42 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,7 +8,7 @@ class Scraper
   
   def get_page
     Nokogiri::HTML(open('http://learn-co-curriculum.github.io/site-for-scraping/courses'))
-    doc.css('.post').each do |post|
+    .css('.post').each do |post|
       course = Course.new
       course.title = post.css('h2').text.strip
       course.schedule = post.css('.date').text.strip
